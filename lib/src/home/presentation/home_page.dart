35625e881cb670cc4e9877a02b505bc293424de6
sanjaysagar12 <me@sanjaysagar.com>
2025-10-23 11:58:44 +0530

    Add routing functionality and home page implementation
    - Introduced route management with AppRoutes class.
    - Created HomePage with navigation to SecondPage.
    - Removed default home widget to utilize named routes.
    - Added initial route handling in main.dart.

---
+++ lib/src/home/presentation/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('This is the Home Page'),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/second'),
+              child: const Text('Go to Second Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
