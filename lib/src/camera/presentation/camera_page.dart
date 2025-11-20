1fdc0f6a75a83ba8b5c55e88603bcd0482e29c31
sanjaysagar12 <me@sanjaysagar.com>
2025-11-20 11:40:11 +0530

    feat(navbar): implement animated visibility for bottom navigation bar based on scroll feat(camera): convert CameraPage to StatefulWidget and enhance UI feat(feed): convert FeedPage to StatefulWidget and enhance UI

--- lib/src/camera/presentation/camera_page.dart
+++ lib/src/camera/presentation/camera_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import '../../../core/theme/app_colors.dart';
 
-class CameraPage extends StatelessWidget {
+class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
 
   @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container(child: const Center(child: Text("Camera Page",style: TextStyle(color: Colors.white),)));
+    return Scaffold(
+      backgroundColor: AppColors.black,
+      body: const Center(
+        child: Text(
+          'Camera Page',
+          style: TextStyle(
+            color: AppColors.pureWhite,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/src/feed/presentation/feed_page.dart
+++ lib/src/feed/presentation/feed_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import '../../../core/theme/app_colors.dart';
 
-class FeedPage extends StatelessWidget {
+class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
 
   @override
+  State<FeedPage> createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Feed Page",style: TextStyle(color: Colors.white),));
+    return Scaffold(
+      backgroundColor: AppColors.black,
+      body: const Center(
+        child: Text(
+          'Feed Page',
+          style: TextStyle(
+            color: AppColors.pureWhite,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
