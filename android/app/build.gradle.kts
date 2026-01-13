2135d2a8f73ddd3f927d906046b52fa612a10daa
dharshan-kumarj <jdharshankumar18@gmail.com>
2026-01-13 15:49:03 +0530

    updated package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.heracle"
+    namespace = "com.heracle.fit"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.heracle"
+        applicationId = "com.heracle.fit"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
