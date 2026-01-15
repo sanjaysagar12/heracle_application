21e066050c9ace9c43952c3baeb7cde8f0e0260b
harsshhan <harshanmathi06@gmail.com>
2026-01-15 18:21:33 +0530

    updated nav bar height

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -15,7 +15,7 @@ class FloatingNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 20, left: 16, right: 16),
+      padding: const EdgeInsets.only(bottom: 60, left: 16, right: 16),
       child: Align(
         alignment: Alignment.bottomCenter,
         child: Container(
